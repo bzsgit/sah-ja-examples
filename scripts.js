676dfbc8b44320990815f196dcb9f921e18e790e
bzsgit <64092143+bzsgit@users.noreply.github.com>
2020-04-27 18:55:22 +0200

    Gyak1

--- scripts.js
+++ scripts.js
@@ -1,2 +1,10 @@
 let name = "Zsu"
 console.log(name+"'s apples")
+$("h1").text("We are having a break")
+$("p").html("StayAtHome Coding Camp Likes: <strong>13</strong>")
+$("strong").css("background", "lime")
+$("strong").css("padding", "5px")
+$("h2").toggleClass("blue")
+$("h2").toggleClass("blue")
+$("ul").append("<li id="special-list-item" >My name is " + name + "</li>")
+$("#special-list-item").remove()
