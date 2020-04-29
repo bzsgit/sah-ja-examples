0b9522f6cc6d310ddb4a6ae77b5ee76205d6af53
bzsgit <64092143+bzsgit@users.noreply.github.com>
2020-04-29 13:28:13 +0200

    Add third part

--- scripts.js
+++ scripts.js
@@ -4,7 +4,27 @@ $("h1").text("We are having a break")
 $("p").html("StayAtHome Coding Camp Likes: <strong>13</strong>")
 $("strong").css("background", "lime")
 $("strong").css("padding", "5px")
+
+
 $("h2").toggleClass("blue")
 $("h2").toggleClass("blue")
-$("ul").append("<li id="special-list-item" >My name is " + name + "</li>")
-$("#special-list-item").remove()
+
+$("ul").append("<li>My name is "+name+"</li>");
+$("ul").append("<li id='special-item'>Removable "+name+"</li>");
+
+$("#special-item").remove()
+
+let addButtonHandler = () => {
+    if ($("li").length < 5) {$("ul").append("<li>My name is "+ name +"</li>")}}
+
+$("#add-button").click(addButtonHandler)
+$("#remove-button").click(()=> {
+    $("li:last-child").remove()
+})
+
+let age = 12
+
+if(age < 18){
+    console.log("Not old enough to drink")} else{
+        console.log("Old enough to drink")}
+
